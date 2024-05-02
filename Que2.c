99886a0121c4e8351b23b2801cfddb2ae6391bb1
divyangvaghela <163798272+divyangvaghela@users.noreply.github.com>
2024-05-02 09:58:26 +0530

    Create Que2.c

---
+++ Que2.c
@@ -0,0 +1,23 @@
+// creat a c program to find the largest element in 1D array
+
+#include <stdio.h>
+
+int main() {
+    int arr[10] = {3, 5, 2, 7, 8, 1, 9, 4, 6, 10};
+    int max = arr[0];
+
+    for (int i = 1; i < 10; i++) {
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+    }
+
+    printf("The largest element in the array is: %d", max);
+
+    return 0;
+}
+
+
+The largest element in the array is: 10
+
+=== Code Execution Successful ===
