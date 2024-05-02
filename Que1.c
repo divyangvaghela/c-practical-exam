42b3b8731611d1e12f680703a10cec8bc54b368c
divyangvaghela <163798272+divyangvaghela@users.noreply.github.com>
2024-05-02 09:56:25 +0530

    Update Que1.c

--- Que1.c
+++ Que1.c
@@ -1,1 +1,34 @@
+// creat a c program to cheak if a character entered by a user is vowel or consonant using a switch statement
 
+#include <stdio.h>
+
+int main() {
+    char ch;
+    printf("Enter a character: ");
+    scanf(" %c", &ch);
+
+    switch(ch) {
+        case 'a':
+        case 'e':
+        case 'i':
+        case 'o':
+        case 'u':
+        case 'A':
+        case 'E':
+        case 'I':
+        case 'O':
+        case 'U':
+            printf("%c is a vowel.\n", ch);
+            break;
+        default:
+            printf("%c is a consonant.\n", ch);
+    }
+
+    return 0;
+}
+
+
+Enter a character: a
+a is a vowel.
+
+=== Code Execution Successful ===
